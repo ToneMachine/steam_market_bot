3288eaf0a56aea9547971d91a0115ad4466b448b
ToneMachine <144462210+ToneMachine@users.noreply.github.com>
2023-12-06 09:34:05 -0500

    clean selen_steam_cards.py

--- selen_steam_cards.py
+++ selen_steam_cards.py
@@ -6,20 +6,22 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
+driver = webdriver.Chrome(service=ChromeService())
+url = 'https://store.steampowered.com/login/?steamtv'
+tos = True
+
 # waits for element to load
 def wait_for(element,elem_name,time = 30):
     WebDriverWait(driver,time).until(EC.presence_of_element_located((element,elem_name)))
 
 # opens webpage
-driver = webdriver.Chrome(service=ChromeService())
-url = 'https://store.steampowered.com/login/?steamtv'
 driver.get(url)
 driver.maximize_window()
 
 # login
 time.sleep(2)
 user = driver.find_element(By.CLASS_NAME,'newlogindialog_TextInput_2eKVn')
-user.send_keys(user) # input(user name)
+user.send_keys(user name) # input(user name)
 password = driver.find_element(By.XPATH,'//*[@id="responsive_page_template_content"]/div[3]/div[1]/div/div/div/div[2]/div/form/div[2]/input')
 password.send_keys(password,Keys.ENTER) # input(password)
 
@@ -31,19 +33,16 @@ driver.find_element(By.ID,'inventory_link_753').click() # input(which game)
 time.sleep(2)
 
 # cycles items
-tos = True
-css_sel = '#inventory_76561198213304301_753_6 > div:nth-child(1) > div:nth-child(2)'
-
 while True:
-    wait_for(By.CSS_SELECTOR,css_sel)
-    driver.find_element(By.CSS_SELECTOR,css_sel).click()
+
+    wait_for(By.CSS_SELECTOR,'#inventory_76561198213304301_753_6 > div:nth-child(1) > div:nth-child(2)')
+    driver.find_element(By.CSS_SELECTOR,'#inventory_76561198213304301_753_6 > div:nth-child(1) > div:nth-child(2)').click()
     wait_for(By.CLASS_NAME,'item_market_actions') 
     tags = driver.find_element(By.ID,'iteminfo1_item_tags_content').text.split(',')
 
     # checks tags
     for tag in tags:
-
-            # turning cards into gems is buggy
+        
         # if tag == ' Not Marketable':
     
         #     wait_for(By.ID,'iteminfo0_item_scrap_link')
@@ -78,5 +77,8 @@ while True:
             time.sleep(2)
             driver.find_element(By.PARTIAL_LINK_TEXT,'OK').click()
             time.sleep(3)
+
+        else:
+            driver.close()
 
 time.sleep(300)
